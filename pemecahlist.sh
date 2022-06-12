2bbabe8a2359c34177fadded14d086cfcbd97864
AlvaGans <105116732+AlvaGans@users.noreply.github.com>
2022-06-12 14:06:38 +0700

    Add files via upload

---
+++ pemecahlist.sh
@@ -0,0 +1,3 @@
+z="
+";fz='ne $';Iz='X-Fe';dz='t -l';Ez=';Pem';Mz=' -p ';az='0): ';Qz='" li';Cz=' "\0';Lz='read';cz='spli';Az='echo';Tz=' Dip';Jz='ll\a';Rz='st';Wz='Line';Dz='33]0';Yz='1000';bz='ne';Hz='t ~ ';Bz=' -en';Fz='ecah';Pz='le: ';Xz='?(0-';Vz='apa ';Gz=' Lis';ez=' $li';Kz='"';gz='list';Sz='"Mau';Oz='t Fi';Uz=' Ber';Nz='"Lis';Zz='0000';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$z$Lz$Mz$Nz$Oz$Pz$Qz$Rz$z$Lz$Mz$Sz$Tz$Fz$Uz$Vz$Wz$Xz$Yz$Zz$az$Qz$bz$z$cz$dz$ez$fz$gz"
